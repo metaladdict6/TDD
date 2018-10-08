589ad7b61bc6c8cf8d179e83447f0423874a0e0a
Robert <r.r.c.ziengs@gmail.com>
2018-10-08 14:54:06 +0200

    Added tests and gitignore

--- test/SpiderSpec.java
+++ test/SpiderSpec.java
@@ -1,5 +1,22 @@
+import org.junit.Test;
+
 /**
  * Created by rrczi on 8-10-2018.
  */
 public class SpiderSpec {
+
+    @Test
+    public void makesLegalMove() {
+
+    }
+
+    @Test
+    public void makeIllegalMove() {
+
+    }
+
+    @Test
+    public void placePiece() {
+        
+    }
 }

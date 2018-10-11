fc2c0fd4c5687077b7f1e151f857ee4197ab9321
Robert <r.r.c.ziengs@gmail.com>
2018-10-11 12:19:08 +0200

    Unit tests af

--- test/HiveSpec.java
+++ test/HiveSpec.java
@@ -13,8 +13,16 @@ import java.util.HashMap;
 public class HiveSpec {
 
     @Test(expected = Hive.IllegalMove.class)
-    public void moveToSpaceWithoutNeighbours() {
-
+    public void moveToSpaceWithoutNeighbours() throws Exception {
+        Game game = new Game();
+        HashMap<Integer, HashMap<Integer, Cell>> grid = game.getGrid();
+        grid.get(0).get(2).add(Game.Player.BLACK, Game.Tile.QUEEN_BEE);
+        grid.get(0).get(3).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(1).get(1).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(1).get(0).add(Game.Player.WHITE, Game.Tile.QUEEN_BEE);
+        grid.get(-1).get(2).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(-1).get(3).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        game.move(3, -1, 3, -2);
     }
 
 

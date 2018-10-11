b20e94caa9691409e7ca993ba19d6d084ff72ad5
Robert <r.r.c.ziengs@gmail.com>
2018-10-11 12:09:12 +0200

    wat unit tests geimplementeerd

--- test/HiveSpec.java
+++ test/HiveSpec.java
@@ -1,6 +1,7 @@
 import nl.hanze.hive.Cell;
 import nl.hanze.hive.Game;
 import nl.hanze.hive.Hive;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -21,14 +22,24 @@ public class HiveSpec {
     public void moveToTwoLevelsInOneMove() throws Exception {
         Game game = new Game();
         HashMap<Integer, HashMap<Integer, Cell>> grid = game.getGrid();
-        grid.get(0).get(-3).add(Game.Player.WHITE, Game.Tile.SPIDER);
-        grid.get(0).get(-3).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(-2).get(-1).add(Game.Player.WHITE, Game.Tile.QUEEN_BEE);
+        grid.get(-3).get(0).add(Game.Player.WHITE, Game.Tile.SPIDER);
+        grid.get(-3).get(0).add(Game.Player.WHITE, Game.Tile.BEETLE);
         game.move(-3, 0, -2, 0);
     }
 
     @Test
     public void playerIsWinner() {
-
+        Game game = new Game();
+        HashMap<Integer, HashMap<Integer, Cell>> grid = game.getGrid();
+        grid.get(0).get(2).add(Game.Player.BLACK, Game.Tile.QUEEN_BEE);
+        grid.get(0).get(1).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(0).get(3).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(1).get(1).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(1).get(2).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(-1).get(2).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(-1).get(3).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        Assert.assertTrue(game.isWinner(Hive.Player.WHITE));
     }
 
     @Test
@@ -38,18 +49,43 @@ public class HiveSpec {
 
     @Test
     public void gameIsDraw() {
-
+        Game game = new Game();
+        HashMap<Integer, HashMap<Integer, Cell>> grid = game.getGrid();
+        // Setting up the white win scenario
+        grid.get(0).get(1).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(0).get(2).add(Game.Player.BLACK, Game.Tile.QUEEN_BEE);
+        grid.get(0).get(3).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(1).get(1).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(1).get(2).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(-1).get(2).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(-1).get(3).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        // Setting up the black win scenario
+        grid.get(0).get(0).add(Game.Player.BLACK, Game.Tile.BEETLE);
+        grid.get(0).get(-1).add(Game.Player.WHITE, Game.Tile.QUEEN_BEE);
+        grid.get(0).get(-2).add(Game.Player.BLACK, Game.Tile.BEETLE);
+        grid.get(1).get(-1).add(Game.Player.BLACK, Game.Tile.BEETLE);
+        grid.get(1).get(-2).add(Game.Player.BLACK, Game.Tile.BEETLE);
+        grid.get(-1).get(0).add(Game.Player.BLACK, Game.Tile.BEETLE);
+        grid.get(-1).get(-1).add(Game.Player.BLACK, Game.Tile.BEETLE);
+        Assert.assertTrue(game.isDraw());
     }
 
     @Test(expected = Hive.IllegalMove.class)
     public void moveBeforeQueenPlacement() throws Exception {
         Game game = new Game();
-
-        game.move(0, 0, 0, 0);
+        HashMap<Integer, HashMap<Integer, Cell>> grid = game.getGrid();
+        grid.get(0).get(0).add(Game.Player.BLACK, Game.Tile.QUEEN_BEE);
+        grid.get(0).get(-1).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        game.move(-1, 0, 0, 0);
     }
 
     @Test(expected = Hive.IllegalMove.class)
-    public void breakTileChain() {
-
+    public void breakTileChain() throws Exception {
+        Game game = new Game();
+        HashMap<Integer, HashMap<Integer, Cell>> grid = game.getGrid();
+        grid.get(0).get(0).add(Game.Player.BLACK, Game.Tile.QUEEN_BEE);
+        grid.get(0).get(-1).add(Game.Player.WHITE, Game.Tile.BEETLE);
+        grid.get(0).get(-2).add(Game.Player.WHITE, Game.Tile.QUEEN_BEE);
+        game.move(-1, 0, 0, 1);
     }
 }

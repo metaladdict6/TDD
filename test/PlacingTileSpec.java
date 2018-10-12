16a7995f3c4089725f2f8bf7a4edfa5e2671a1f0
Robert <r.r.c.ziengs@gmail.com>
2018-10-12 12:57:36 +0200

    movement unit tests

--- test/PlacingTileSpec.java
+++ test/PlacingTileSpec.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 
 /**
  * Created by rrczi on 11-10-2018.
+ * This class tests if illegal placements can occure in the game.
  */
 public class PlacingTileSpec {
 
@@ -26,5 +27,14 @@ public class PlacingTileSpec {
         grid.get(-2).get(-1).add(Game.Player.WHITE, Game.Tile.QUEEN_BEE);
         grid.get(-3).get(0).add(Game.Player.WHITE, Game.Tile.SPIDER);
         game.play(Hive.Tile.BEETLE, 0, 0);
+    }
+
+    @Test(expected =  Hive.IllegalMove.class)
+    public void placeNextToOpponent() throws Exception {
+        Game game = new Game();
+        HashMap<Integer, HashMap<Integer, Cell>> grid = game.getGrid();
+        grid.get(-3).get(0).add(Game.Player.WHITE, Game.Tile.SPIDER);
+        game.play(Game.Tile.SOLDIER_ANT, -1, -3);
+
     }
 }
